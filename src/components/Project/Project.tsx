884e73e279ac8020404a37fb55c7185a7d22010a
Vivek maurya <vivek563maurya@gmail.com>
2023-10-27 01:51:50 +0530

    "updated project section"

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,10 +1,133 @@
 import React from "react";
+import Card from "./Card";
 
 function Project() {
   return (
     <>
-    <h2 style={{marginTop:"10rem",textAlign:"center"}}>Updates are currently underway. <br/> We will provide a comprehensive update shortly.</h2>
-     {/* <div className="project-card-design">
+    {/* <h2 >Updates are currently underway. <br/> We will provide a comprehensive update shortly.</h2> */}
+    <section className="section blog bg-grey">
+        <div className="section-title">
+          <h2>Android Project</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="cardstyle">
+        <Card 
+              title="BCA NOTES (Android App)"
+              projectdiscription="Build your Knowledge skills with the BCA Application. Learn the subject of BCA with a proper notes or become an expert of computer with this best BCA learning app. Learn to code with C Language for free with a many notes of BCA for learning app. If you’re reparing to take admission in BCA or doing BCA from any college then this BCA app will help you in ever for PREPARING NOTES clearing every problems."
+              sourcelink="https://play.google.com/store/apps/details?id=com.vivek.bcanotes&hl=en&gl=US&pli=1"
+              livelink="https://play.google.com/store/apps/details?id=com.vivek.bcanotes&hl=en&gl=US&pli=1"
+              
+          /> 
+        <Card 
+              title="Women Safety"
+              projectdiscription="The Women Safety Android Application is a powerful and user-centric mobile app designed to provide women with a reliable and immediate safety solution. Built using Java and Firebase, this application addresses the critical issue of women's safety by incorporating an SOS feature."
+              sourcelink="https://github.com/Vivek563/wsafety"
+              livelink="https://github.com/Vivek563/wsafety"
+              
+          /> 
+          
+        </div>
+        <br/>
+        <br/><br/>
+        <br/>
+        <div className="section-title">
+          <h2>React Project</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="cardstyle">
+          
+          
+          <Card 
+              title="Blog Application Using Appwrite"
+              projectdiscription="The Blog React App with Appwrite Database is a powerful and versatile web application that seamlessly combines the user-friendly interface of a React front-end with the robust capabilities of the Appwrite database to provide a comprehensive platform for creating, managing, and sharing blog content."
+              sourcelink="https://github.com/Vivek563/React-Blog-Application-Using-Appwrite"
+              livelink="https://github.com/Vivek563/React-Blog-Application-Using-Appwrite"
+              
+          /> 
+           <Card 
+              title="Notes Taking"
+              projectdiscription="I led a ReactJS project, integrating Object-Oriented JavaScript, Material UI, and Context API, with a focus on SDLC, robust data storage, QA, and security. Our mobile optimization and version control followed Agile methodologies, highlighting client-centricity. I showcased adaptability, independence, and a dedication to technical excellence."
+              sourcelink="https://github.com/Vivek563/Notes-taking"
+              livelink="https://todonotesmaking.netlify.app/"
+              
+          /> 
+          
+          <Card 
+              title="Digital FingerPrint"
+              projectdiscription="Digital Fingerprint is an open-source web application powered by Vite and React, designed for seamless visitor data analysis. It offers real-time insights into geographical specifics, including IP address, city, country, ISP, and more. Additionally, it provides comprehensive device details such as browser, operating system, screen resolution, and installed plugins. With its user-friendly interface and minimalist design."
+              sourcelink="https://github.com/Vivek563/React-Blog-Application-Using-Appwrite"
+              livelink="https://github.com/Vivek563/React-Blog-Application-Using-Appwrite"
+              
+          />  
+          <Card 
+              title="Outlook Email Clone"
+              projectdiscription="Experience the power of our React and Redux-based email client app, reminiscent of Outlook. Effortlessly manage your emails, view detailed email information, and take action with ease. Enjoy a sleek email list page with pagination, read/unread styling, and filter emails by favorites or status. Plus, stay organized with a user-friendly date format (dd/MM/yyyy hh:mm a). Email management has never been this seamless!"
+              sourcelink="https://github.com/Vivek563/Outlook-email-Clone-React-app"
+              livelink="https://vivekoutlookmail.netlify.app/"
+              
+          />  
+          
+          
+          <Card 
+              title="Personal Portfolio"
+              projectdiscription="Welcome to my personal website, a dynamic online portfolio that showcases my skills, projects, and more. This website is built using a React-Typescript Vite application, providing a fast and efficient user experience.."
+              sourcelink="https://github.com/Vivek563/Portfolio-React-Typescript"
+              livelink="https://vivekmaurya.me"
+              
+          /> 
+           <Card 
+              title="All React Project"
+              projectdiscription="This repository contains a collection of 10 React applications created while learning from the YouTube channel Chai and Code by Hitesh Choudary. Each application is built using Vite as the bundler and React as the JavaScript library."
+              sourcelink="https://github.com/Vivek563/React.js-Practice-Projects"
+              livelink="https://github.com/Vivek563/React.js-Practice-Projects"
+              
+          /> 
+          
+          <Card 
+              title="Color Picker"
+              projectdiscription="This is a project for creating a colorpicker tool. To use the colorpicker, simply move the cursor over the color card to select a color and click there it will automatically copy the color hex code to the clipboard."
+              sourcelink="https://github.com/Vivek563/ColorPicker"
+              livelink="https://osmcolor.netlify.app/"
+              
+          /> 
+         
+        </div>
+        
+        <br/>
+        <br/><br/>
+        <br/>
+        <div className="section-title">
+          <h2>Full-Stack Project</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="cardstyle">
+
+        <Card 
+              title="Institute of Professional Studies (College Website)"
+              projectdiscription="I spearheaded college website development with MVC architecture, a client-centric focus, agile methodology, and Node.js/Express.js for the back-end. I ensured seamless integration of a user-friendly CMS and refined EJS front-end. My strengths included debugging, optimization, team collaboration, and quality assurance through automated testing. The result: an exceptional, cross-device performance website."
+              sourcelink="https://github.com/Vivek563/Institute-of-Professional-Studies"
+              livelink="https://github.com/Vivek563/Institute-of-Professional-Studies"
+              
+          /> 
+          <Card 
+              title="URL Shortener"
+              projectdiscription="Introducing my custom URL shortener app, a sleek and efficient solution crafted with HTML, CSS, JavaScript, and EJS. Simplifying long, cumbersome links into concise, user-friendly URLs, this application offers a seamless browsing experience. Experience the convenience of effortless link sharing and tracking, all wrapped in an elegant, user-friendly design. Say goodbye to unwieldy URLs and embrace the power of simplicity with this intuitive URL shortener."
+              sourcelink="https://github.com/Vivek563/Url-Shortner"
+              livelink="https://github.com/Vivek563/Url-Shortner"
+              
+          /> 
+          
+        </div>
+
+        
+        <div style={{ textAlign: "center", marginTop: "30px" }}>
+          <h1>Want to See All My Work?</h1>
+          <h4>
+            <a href="https://github.com/vivek563" target="blank">Click Me🙃</a>
+          </h4>
+        </div>
+      </section>
+     {/* <div style={{marginTop:"10rem",textAlign:"center"}} className="project-card-design">
       <div className="project-design-container">
         <div className="drop" >
           <div className="project-design-content">
